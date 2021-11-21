a3cbdaf99044f053a866355d6739fb280fc0a4c7
by-German <german280599@gmail.com>
2021-11-20 23:08:53 -0500

    base path for isw backend

--- src/app/services/common/http.common.ts
+++ src/app/services/common/http.common.ts
@@ -1,3 +1,4 @@
 // export const BASE_PATH = "https://api-frikiteam.herokuapp.com/api";
-export const BASE_PATH = "http://localhost:8080/frikiteam-backend/api";
+// export const BASE_PATH = "http://localhost:8080/frikiteam-backend/api";
+export const BASE_PATH = "https://frikiteam-isw.herokuapp.com/api";
 // export const BASE_PATH = "http://localhost:8080/frikiteam-backend/api";
